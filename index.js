9b38d3768d6f29260386e0071c5b3a4def8e2fe3
Lucas Griffin <lucasgriffin89@gmail.com>
2022-07-06 22:44:26 +1000

    set-up

---
+++ index.js
@@ -0,0 +1,48 @@
+const questions = [{
+    question: 'question one',
+    answer: 'answer 1',
+    answer: 'answer 2',
+    answer: 'answer 3',
+    answer: 'answer 4',
+},
+{
+    question: 'question 2',
+    answer: 'answer 1',
+    answer: 'answer 2',
+    answer: 'answer 3',
+    answer: 'answer 4'
+}]
+
+score = 0
+
+let question = document.getElementById('question')
+let answerDiv = document.getElementById('answer')
+let start = document.getElementById('start')
+let game = document.getElementById('game')
+let startGameButton = document.getElementById('start-button')
+let restartGameButton = document.getElementById('restart-button')
+
+startGameButton.addEventListener('click', startGame)
+restartGameButton.addEventListener('click', restartGame)
+
+function startGame() {
+    console.log('start game')
+    start.classList.add('hide') 
+    game.classList.remove('hide') 
+    
+}
+
+function correctAnswer(){
+    
+}
+
+function wrongAnswer() {
+    
+}
+function restartGame() {
+    console.log('RE-start game')
+    start.classList.remove('hide') 
+    game.classList.add('hide') 
+
+}
+
